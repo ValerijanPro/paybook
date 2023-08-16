315c3712126cd77204ccb7515ba7d9faa242b322
nikolababic11 <nikola.babic@vyking.com>
2023-08-17 00:42:58 +0200

    full screen qr scan

--- src/components/QRCodeReader.js
+++ src/components/QRCodeReader.js
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useZxing } from "react-zxing";
 import styles from "../styles/QRCodeReader.module.css";
 
 const QRCodeReader = () => {
+  const [result, setResult] = useState();
   const { ref } = useZxing({
     onResult(result) {
-      window.open(result.getText(), "_blank");
+      setResult(result.getText());
     },
   });
 
+  useEffect(() => {
+    if (result) {
+      window.open(result, "_blank");
+    }
+  }, [result]);
+
   return (
     <div className={styles.container}>
-      <video ref={ref} />
+      <video className={styles.camera} ref={ref} />
     </div>
   );
 };
