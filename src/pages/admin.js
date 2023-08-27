e7a44f9330eacbc5a32a7ca1d657a0889dc1fd59
nikolababic11 <nikola.babic@vyking.com>
2023-08-27 11:50:22 +0200

    tryLogin

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import styles from "../styles/Admin.module.css";
 
-import {AiOutlineUser, AiFillLock} from "react-icons/ai"
+import { AiOutlineUser, AiFillLock } from "react-icons/ai";
 
 function AdminPage() {
   const [username, setUsername] = useState("");
@@ -16,7 +16,7 @@ function AdminPage() {
   const router = useRouter();
 
   useEffect(() => {
-    console.log("fired")
+    console.log("fired");
     if (showErrorMessage && (!isMovingRight || isMovingLeft)) {
       setIsMovingRight(true);
       setIsMovingLeft(false);
@@ -25,27 +25,22 @@ function AdminPage() {
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
-    if(password!="")
-      {
-        setShowErrorMessage(false);
-      setIsMovingRight(true);
-      setIsMovingLeft(false);
-      }
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-    if(username!=""){
+    if (password != "") {
       setShowErrorMessage(false);
       setIsMovingRight(true);
       setIsMovingLeft(false);
     }
-     
-
   };
 
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    if (username != "") {
+      setShowErrorMessage(false);
+      setIsMovingRight(true);
+      setIsMovingLeft(false);
+    }
+  };
 
-  
   const handleButtonClick = () => {
     if (!username || !password) {
       setShowErrorMessage(true);
@@ -59,67 +54,54 @@ function AdminPage() {
         setIsMovingRight(true);
       }
     } else {
-      
       // Implement your login logic here
-      tryLogin().then(()=>{
-        setTimeout(()=>{
-          console.log("Error ovde"+showErrorMessage);
-          if(showErrorMessage) return;
-          setShowErrorMessage(false);
-          setIsMovingRight(true);
-          setIsMovingLeft(false);
-          setShowLoadingIcon(true);
-          setShowErrorMessage(false);
-          //delay 1s onako cisto;
-          setTimeout(() => {
-            console.log("Logged in bro!");
-            //ASK:
-            //staviti u neki sessionStorage user-a?
-            router.push('/orders');
-          }, 1000);
-          console.log("Logged in bro!");
-        }, 1000)
-       
-        
-      });
+      tryLogin();
     }
   };
 
   const tryLogin = async () => {
     try {
-      const response = await fetch('https://arliving.herokuapp.com/arliving/pb_log_in_restaurant', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+      const response = await fetch(
+        "https://arliving.herokuapp.com/arliving/pb_log_in_restaurant",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
 
-        body: JSON.stringify({
-          user_name: username,
-          password: password
-          //ovo otkomentarisi kad API dobijes od Paje
-          /*
+          body: JSON.stringify({
+            user_name: username,
+            password: password,
+            //ovo otkomentarisi kad API dobijes od Paje
+            /*
           username: 'username',
           password: 'password'*/
-        }),
-        
-      });
-
-      
+          }),
+        }
+      );
 
       const responseData = await response.json();
       if (responseData.error) {
         console.log("nema brt");
         setShowErrorMessage(true);
-        setShowErrorMessage(true);
-        console.log("Hladno mi je "+showErrorMessage)
-        return;
+        console.log("Hladno mi je " + showErrorMessage);
+      } else {
+        setUser(responseData);
+        console.log("Successful login!");
+        console.log(responseData.id);
+        sessionStorage.setItem("restaurant", JSON.stringify(responseData));
+        console.log(sessionStorage.getItem("restaurant"));
+        setIsMovingRight(true);
+        setIsMovingLeft(false);
+        setShowLoadingIcon(true);
+        //delay 1s onako cisto;
+
+        //ASK:
+        //staviti u neki sessionStorage user-a?
+        router.push("/orders");
+
+        console.log("Logged in bro!");
       }
-      setUser(responseData);
-      console.log("Successful login!");
-      console.log(responseData.id);
-      sessionStorage.setItem("restaurant", JSON.stringify(responseData));
-      console.log(sessionStorage.getItem("restaurant"))
-      
     } catch (error) {
       //setShowErrorMessage('An error occurred while fetching data');
     }
@@ -127,16 +109,13 @@ function AdminPage() {
 
   return (
     <div className={styles.container}>
-      
       <form>
-      
         <div className={styles.formGroup}>
           <label htmlFor="username" className={styles.inputLabel}>
             <div className={styles.labelRow}>
-              <AiOutlineUser style={{ height:'100%' , width:'100%'}} /> 
-              <div className={styles.inputText}>Username:</div> 
+              <AiOutlineUser style={{ height: "100%", width: "100%" }} />
+              <div className={styles.inputText}>Username:</div>
             </div>
-            
           </label>
           <div className={styles.inputWrapper}>
             <input
@@ -151,8 +130,8 @@ function AdminPage() {
         <div className={styles.formGroup}>
           <label htmlFor="password" className={styles.inputLabel}>
             <div className={styles.labelRow}>
-                <AiFillLock style={{ height:'100%' , width:'100%'}} /> 
-                <div className={styles.inputText}>Password:</div> 
+              <AiFillLock style={{ height: "100%", width: "100%" }} />
+              <div className={styles.inputText}>Password:</div>
             </div>
           </label>
           <div className={styles.inputWrapper}>
@@ -166,29 +145,41 @@ function AdminPage() {
           </div>
         </div>
         {showErrorMessage && (
-          <p style={{ marginLeft: '5px',  marginRight: '4px',  marginBottom:'-15px', display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center' }}
+          <p
+            style={{
+              marginLeft: "5px",
+              marginRight: "4px",
+              marginBottom: "-15px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
             className={`${styles.error} ${isMovingRight ? styles.moveUp : ""} ${
               isMovingLeft ? styles.moveDown : ""
             }`}
           >
-            <span className={styles.textCenter}>Please enter both username and password</span>
+            <span className={styles.textCenter}>
+              Please enter both username and password
+            </span>
           </p>
         )}
         <div className={styles.loadingWrapper}>
           {showLoadingIcon && (
             <div className={styles.wineGlassesWrapper}>
-              <div className={`${styles.wineGlass} ${styles.leftWineGlass}`}></div>
-              <div className={`${styles.wineGlass} ${styles.rightWineGlass}`}></div>
+              <div
+                className={`${styles.wineGlass} ${styles.leftWineGlass}`}
+              ></div>
+              <div
+                className={`${styles.wineGlass} ${styles.rightWineGlass}`}
+              ></div>
             </div>
           )}
         </div>
         <button
           type="button"
-          className={`${styles.loginButton} ${isMovingRight ? styles.moveDown : ""} ${
-            isMovingLeft ? styles.moveRight : ""
-          }`}
+          className={`${styles.loginButton} ${
+            isMovingRight ? styles.moveDown : ""
+          } ${isMovingLeft ? styles.moveRight : ""}`}
           onClick={handleButtonClick}
         >
           Login
@@ -196,7 +187,6 @@ function AdminPage() {
       </form>
     </div>
   );
-  
 }
 
 export default AdminPage;
